e348162637bbd40cb3f0f67989baec0ae5466fd0
Jason Terk <jason@goterkyourself.com>
2020-12-07 18:26:52 -0800

    Simplify Day 7 & Finish

--- 07-handy-haversacks/solution.py
+++ 07-handy-haversacks/solution.py
@@ -14,102 +14,82 @@ vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags."""
 
-class Bag:
-    def __init__(self, color):
-        self.color = color
-        self.contains = dict()
-        self.contained_by = set()
+SAMPLE2= """shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags."""
 
-    def __str__(self):
-        return self.color
+BAG_SPEC = re.compile('^([a-z ]+) bags contain ([^.]*).$')
+CONTAINED_SPEC = re.compile('^([0-9]+) ([a-z ]+) bags?$')
 
-    def __hash__(self):
-        return hash(self.color)
+def add_rule(rules, rule_str):
+    m = BAG_SPEC.match(rule_str)
+    color = m.group(1)
+    contains = {}
 
-    def add_container(self, container):
-        #print('Adding container {} for {}'.format(container.color, self.color))
-        self.contained_by.add(container)
-        #print(self.contained_by)
+    assert color not in rules
 
-    def add_contents(self, contained, count):
-        self.contains[contained] = count
+    for contained in m.group(2).split(', '):
+        if contained.startswith('no other'):
+            continue
 
-    def containers(self):
-        return self.contained_by
+        m = CONTAINED_SPEC.match(contained)
+        contains[m.group(2)] = m.group(1)
 
-class BagColors:
-    BAG_SPEC = re.compile('^([a-z ]+) bags contain ([^.]*).$')
-    CONTAINED_SPEC = re.compile('^([0-9]+) ([a-z ]+) bags?$')
+    rules[color] = contains
 
-    bags = dict()
+def count_containers(rules, color):
+    to_check = set([color])
+    containers = set()
 
-    def add_bag(self, input_line):
-        #print(input_line)
-        m = self.BAG_SPEC.match(input_line)
-        #print(m)
-        #print(m.group(2).split(', '))
+    while len(to_check):
+        contained = to_check.pop()
 
-        if not m:
-            print('{} does not match expected spec'.format(input_line))
-            exit(1)
+        for color, contents in rules.items():
+            if contained in contents and color not in containers:
+                containers.add(color)
+                to_check.add(color)
 
-        color = m.group(1)
+    return len(containers)
 
-        if not color in self.bags:
-            self.bags[color] = Bag(color)
+def count_contained(rules, color):
+    if len(rules[color]):
+        subtotal = 0
 
-        for contained in m.group(2).split(', '):
-            if contained.startswith('no other'):
-                continue
+        for c, count in rules[color].items():
+            subtotal += int(count)
+            subtotal += int(count) * count_contained(rules, c)
 
-            m = self.CONTAINED_SPEC.match(contained)
-
-            if not m:
-                print('{} does not match expected contained spec'.format(contained))
-                exit(1)
-
-            c_count = m.group(1)
-            c_color = m.group(2)
-
-            #print('{} contains {} {}'.format(color, c_count, c_color))
-
-            if not c_color in self.bags:
-                self.bags[c_color] = Bag(c_color)
-
-            self.bags[c_color].add_container(self.bags[color])
-            self.bags[color].add_contents(self.bags[c_color], c_count)
-
-    def add_bags(self, input_lines):
-        for l in input_lines.split('\n'):
-            self.add_bag(l)
-
-    def get_containers(self, color):
-        # Needs to be turned into a loop for real soln
-        bag = self.bags[color]
-        assert bag
-        #print(color)
-        #print([b.color for b in bag.containers()])
-
-        if not len(bag.containers()):
-            return {bag}
-        else:
-            containers = set()
-
-            for b in bag.containers():
-                containers.update({b})
-                containers.update(self.get_containers(b.color))
-
-            return containers
+        return subtotal
+    else:
+        return 0
 
 # Part 1
-SAMPLE_BAGS = BagColors()
-SAMPLE_BAGS.add_bags(SAMPLE)
-assert len(SAMPLE_BAGS.get_containers('shiny gold')) == 4
+SAMPLE_RULES = {}
 
-BAGS = BagColors()
+for rule_str in SAMPLE.split('\n'):
+    add_rule(SAMPLE_RULES, rule_str)
+
+assert count_containers(SAMPLE_RULES, 'shiny gold') == 4
+
+RULES = {}
 
 with open('input', 'r') as f:
     for l in f:
-        BAGS.add_bag(l)
+        add_rule(RULES, l)
 
-print('Bags containing shiny gold: {}'.format(len(BAGS.get_containers('shiny gold'))))
+containers = count_containers(RULES, 'shiny gold')
+print('Bags containing shiny gold: {}'.format(containers))
+assert containers == 112
+
+# Part 2
+SAMPLE2_RULES = {}
+
+for rule_str in SAMPLE2.split('\n'):
+    add_rule(SAMPLE2_RULES, rule_str)
+
+assert count_contained(SAMPLE2_RULES, 'shiny gold') == 126
+print('Shiny gold bags contain {} bags'.format(count_contained(RULES, 'shiny gold')))
