df68f8dcc5fcf21e94bf42a1fdd57c8e6d06ed76
Jason Terk <jason@goterkyourself.com>
2024-12-06 08:00:22 -0800

    anyhow usage in main.rs

--- ac2024/src/main.rs
+++ ac2024/src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::{anyhow, Result};
+
 use std::{
     collections::HashMap,
     env,
@@ -14,18 +16,18 @@ trait Day {
     fn part_2(&self) -> Result<i64, String>;
 }
 
-fn load_dot_env() -> Result<HashMap<String, String>, String> {
-    let file = File::open(".env").map_err(|e| e.to_string())?;
+fn load_dot_env() -> Result<HashMap<String, String>> {
+    let file = File::open(".env")?;
 
     let mut env = HashMap::new();
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
-        let s = line.map_err(|e| e.to_string())?;
+        let s = line?;
         let words: Vec<&str> = s.split('=').collect();
 
         if words.len() != 2 {
-            return Err(format!("Malformed env line {}", s));
+            return Err(anyhow!("Malformed env line {}", s));
         }
 
         env.insert(words[0].to_string(), words[1].to_string());
@@ -34,17 +36,17 @@ fn load_dot_env() -> Result<HashMap<String, String>, String> {
     Ok(env)
 }
 
-fn get_day(day: u8, buf: BufReader<File>) -> Result<Box<dyn Day>, String> {
+fn get_day(day: u8, buf: BufReader<File>) -> Result<Box<dyn Day>> {
     match day {
         1 => Ok(Box::new(day_1::new(buf))),
         2 => Ok(Box::new(day_2::new(buf))),
         3 => Ok(Box::new(day_3::new(buf))),
-        _ => Err(format!("Unknown day {}", day)),
+        _ => Err(anyhow!("Unknown day {}", day)),
     }
 }
 
-fn get_input(env: HashMap<String, String>, day: u8) -> Result<BufReader<File>, String> {
-    fs::create_dir_all("inputs").map_err(|e| e.to_string())?;
+fn get_input(env: HashMap<String, String>, day: u8) -> Result<BufReader<File>> {
+    fs::create_dir_all("inputs")?;
     let filename = format!("inputs/{}", day);
     let f = File::open(filename.clone());
 
@@ -62,21 +64,17 @@ fn get_input(env: HashMap<String, String>, day: u8) -> Result<BufReader<File>, S
                         env.get("SESSION_COOKIE").expect("SESSION_COOKIE not set")
                     ),
                 )
-                .send()
-                .map_err(|e| e.to_string())?;
-            fs::write(filename.clone(), res.text().map_err(|e| e.to_string())?)
-                .map_err(|e| e.to_string())?;
-            Ok(BufReader::new(
-                File::open(filename).map_err(|e| e.to_string())?,
-            ))
+                .send()?;
+            fs::write(filename.clone(), res.text()?)?;
+            Ok(BufReader::new(File::open(filename)?))
         }
     }
 }
 
-fn main() -> Result<(), String> {
+fn main() -> Result<()> {
     let env = load_dot_env()?;
     let args: Vec<String> = env::args().collect();
-    let day_arg = args[1].parse::<u8>().map_err(|e| e.to_string())?;
+    let day_arg = args[1].parse::<u8>()?;
     let input = get_input(env, day_arg)?;
     let day = get_day(day_arg, input)?;
 
